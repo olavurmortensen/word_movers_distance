f32c1298c4893cd37092dbb619471f051a64f7a2
Ólavur Mortensen <olavurmortensen@gmail.com>
2015-11-06 18:00:38 +0100

    Cleaned code and added comments.

--- WMD.py
+++ WMD.py
@@ -37,26 +37,23 @@ def distance(v1, v2):
 
     return dist
 
-def nBOW(document, embeddings):
+def nBOW(document):
     '''
     Compute nBOW representation of a document
 
     Input:
-    doc:                Document, list of words (strings).
-    vocab_idx_dict:     Dictionary, maps words in vocabulary to unique indeces (from 0 to size of vocabulary).
+    doc:        Document, list of words (strings).
 
-    Returns:    Numpy array of same size as vocab_idx_dict, nBOW representation of input document.
+    Returns:    List of floats, nBow representation.
     '''
 
-    freq = {}
-    for token in document:
-        freq[token] = document.count(token)
-    freq_sum = sum(freq.values())
-
+    # Compute the normalized frequency of each token (store in dictionary).
+    norm = len(document)
     d_dict = {}
     for token in document:
-        d_dict[token] = freq[token] / float(freq_sum)
+        d_dict[token] = document.count(token) / float(norm)
 
+    # Procude list with normalized frequency for each token in document.
     d = []
     for i, token in enumerate(document):
         d.append(d_dict[token])
@@ -75,26 +72,30 @@ def WMD(document1, document2, embeddings):
     Returns:        WMD between documents, float.
     '''
 
-    d1 = nBOW(document1, model)
-    d2 = nBOW(document2, model)
-
+    # Compute nBOW representation of documents.
+    d1 = nBOW(document1)
+    d2 = nBOW(document2)
+    
+    # Get features.
     features1 = [tuple(embeddings[token]) for token in document1]
     features2 = [tuple(embeddings[token]) for token in document2]
 
+    # Return WMD.
     return emd((features1, d1), (features2, d1), distance)
 
 if __name__ == '__main__':
     with open('w2v_model.pickle', 'rb') as f:
         model = pickle.load(f)
 
+    # Sentence to compute distance between.
     sentence1 = 'mayor london'.split()
     sentence2 = 'president america'.split()
 
+    # Remove out-of-vocabulary words.
     sentence1 = [token for token in sentence1 if token in model.vocab.keys()]
     sentence2 = [token for token in sentence2 if token in model.vocab.keys()]
 
-    vocab = set([token for token in sentence1 + sentence2])
-
+    # Compute WMD.
     D = WMD(sentence1, sentence2, model)
 
     print D
